7466360a3bb87d6d250707c90db606b5c043ddaf
Onyx Dragoon <john.doe@example.com>
2020-03-07 17:00:47 -0500

    Give a hostname to the docker container.

--- build-and-run.sh
+++ build-and-run.sh
@@ -3,4 +3,12 @@ chmod +x nxserver.sh
 NAME=nm
 docker build -t ${NAME} .
 docker stop ${NAME} && docker rm ${NAME}
-docker run --name ${NAME} -d -p 4001:4000 -p 2222:22 --cap-add=SYS_PTRACE ${NAME}
+
+docker run \
+  -d \
+  --name ${NAME} \
+  --hostname zaks \
+  -p 4001:4000 \
+  -p 2222:22 \
+  --cap-add=SYS_PTRACE \
+  ${NAME}
